21d2742ee79fcdd999e8474ccf688a75190408bd
akhil-roshan <akhilroshan111@gmail.com>
2021-05-27 16:25:12 +0530

    Navbar added

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './NavBar.css'
+
+
+function NavBar() {
+  return (
+    <div className="navbar">
+      <img className="logo" src="./assets/img-digi.png" alt="digi art" />
+      <img className="user_circle" src="./assets/icon_circle.png" alt="" />
+    </div>
+    
+    )
+}
+
+export default NavBar;
